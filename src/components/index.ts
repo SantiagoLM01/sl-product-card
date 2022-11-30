00fdd7c6ab4578dec52514014f6fef3d04f7c376
Santiago <santilizarazo2001@hotmail.com>
2022-11-30 08:40:23 -0600

    Version 0.2.0 Nueva

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,9 @@ import { ProductButtons } from "./ProductButtons";
 import { ProductTitle } from "./ProductTitle";
 import { ProductCardHOCProps } from "../interfaces/interfaces";
 
+export { ProductImage } from "./ProductImage";
+export { ProductButtons } from "./ProductButtons";
+export { ProductTitle } from "./ProductTitle";
 
 export const ProductCard: ProductCardHOCProps = Object.assign(ProductCardHOC , {
     Title: ProductTitle,
